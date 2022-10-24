386dd4a387d59d3d3e65447643cb3899e79d7f98
minos antares <minosantares@gmail.com>
2022-10-24 13:33:50 -0500

    Prueba,24-10

---
+++ Assets/Scripts/Guardado/DatosGuardar.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace data
+{
+    public class DatosGuardar : MonoBehaviour
+    {
+        public int dinero;
+        public float distancia;
+        public GameObject[] naves;
+    }
+}

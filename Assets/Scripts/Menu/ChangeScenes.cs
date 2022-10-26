2895e311ad680fd6ca42dabaa29ec431c6e8fe30
minos antares <minosantares@gmail.com>
2022-10-26 14:04:33 -0500

    Finales

--- Assets/Scripts/Menu/ChangeScenes.cs
+++ Assets/Scripts/Menu/ChangeScenes.cs
@@ -5,8 +5,16 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScenes : MonoBehaviour
 {
+    //SerializeField] private Animator transition;
     public void Jugar()
     {
-        SceneManager.LoadScene("Juego");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
+
+    /*IEnumerator LoadLevel(int leveIndex)
+    {
+        transition.SetTrigger("Start");
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadScene(leveIndex);
+    }*/
 }

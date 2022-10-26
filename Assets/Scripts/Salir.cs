f49d26736532541269e6a8e9fc4eb5416fe88e94
Juan David <judama981@hotmail.com>
2022-10-26 13:27:49 -0500

    Reinicia,sale

---
+++ Assets/Scripts/Salir.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Salir : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SalirApp()
+    {
+        Application.Quit();
+        Debug.Log("se salio");
+    }
+    public void Reiniciar()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        AudioManager.Instance.Musicasource.Play();
+    }
+    public void Inicia()
+    {
+        SceneManager.LoadScene(1);
+        print("carga");
+    }
+}

8158fe1c72d27a92849e9eb0a8831716369faeed
minos antares <minosantares@gmail.com>
2022-10-25 22:16:40 -0500

    Guardar

--- Assets/Scripts/Guardado/GuardarCargar.cs
+++ Assets/Scripts/Guardado/GuardarCargar.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using UnityEngine;
 
@@ -5,14 +6,16 @@ namespace data
 {
     public class GuardarCargar : MonoBehaviour
     {
-       // private const string FileName = "infoPlayer.data";
+       private const string FileName = "infoPlayer.data";
+       public event Action DataLoaded;
         [SerializeField] private DatosGuardar datosGuardar;
+        public DatosGuardar DatosGuardar => datosGuardar;
 
         [ContextMenu("save")]
         public void GuardarDatos()
         {
             string infoJugador = JsonUtility.ToJson(datosGuardar);
-            string path = Path.Combine(Application.persistentDataPath, "infoPlayer.data");
+            string path = Path.Combine(Application.persistentDataPath,  FileName);
             File.WriteAllText(path, infoJugador);
             Debug.Log(JsonUtility.ToJson(datosGuardar));
         }
@@ -20,13 +23,14 @@ namespace data
         [ContextMenu("loaded")]
         public void CargarDatos()
         {
-            string path = Path.Combine(Application.persistentDataPath, "infoPlayer.data");
+            string path = Path.Combine(Application.persistentDataPath,  FileName);
             string infoJugador = File.ReadAllText(path);
             DatosGuardar datosCargados = JsonUtility.FromJson<DatosGuardar>(infoJugador);
             datosGuardar.dinero = datosCargados.dinero;
             datosGuardar.distancia = datosCargados.distancia;
             datosGuardar.ultimaNave = datosCargados.ultimaNave;
             datosGuardar.naves = datosCargados.naves;
+            DataLoaded?.Invoke();
         }
     }
 }
